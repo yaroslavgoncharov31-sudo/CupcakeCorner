97fdffab4769926b4d5a74e63a1c742612dbe9ce
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-12 20:33:05 +0200

    add CupcakeType enum

---
+++ CupcakeCorner/Models/CupcakeType.swift
@@ -0,0 +1,15 @@
+//
+//  CupcakeType.swift
+//  CupcakeCorner
+//
+//  Created by Yaroslav on 9/12/26.
+//
+
+import Foundation
+
+enum CupcakeType: String, CaseIterable {
+    case vanilla = "Vanilla"
+    case chocolate = "Chocolate"
+    case strawberry = "Strawberry"
+    case rainbow = "Rainbow"
+}

ee99f005a68c17917d49607cd49fa4efb82e9965
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-13 12:06:24 +0200

    add cost property to CupcakeType and make it conform to Codable

--- CupcakeCorner/Models/CupcakeType.swift
+++ CupcakeCorner/Models/CupcakeType.swift
@@ -7,9 +7,23 @@
 
 import Foundation
 
-enum CupcakeType: String, CaseIterable {
+enum CupcakeType: String, CaseIterable, Codable {
     case vanilla = "Vanilla"
     case chocolate = "Chocolate"
     case strawberry = "Strawberry"
     case rainbow = "Rainbow"
+
+    var cost: Decimal {
+        switch self {
+        case .vanilla:
+            2.0
+        case .chocolate:
+            2.0
+        case .strawberry:
+            2.5
+        case .rainbow:
+            4.0
+        }
+    }
 }
+

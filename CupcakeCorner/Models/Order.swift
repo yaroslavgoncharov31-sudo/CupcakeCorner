38f694c8502db6c529117eb4e7fe7c0949bca168
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-13 09:12:11 +0200

    add property observer to specialRequestEnabled and add adress details variables & validation

--- CupcakeCorner/Models/Order.swift
+++ CupcakeCorner/Models/Order.swift
@@ -12,7 +12,27 @@ class Order {
     var type: CupcakeType = .chocolate
     var quantity = 3
 
-    var specialRequestEnabled = false
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
     var extraFrosting = false
     var addSprinkles = false
+
+    var name = ""
+    var streetAddress = ""
+    var zip = ""
+    var city = ""
+
+    var hasValidAdress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || zip.isEmpty || city.isEmpty {
+            return false
+        }
+        return true
+    }
+
 }
